ae7156c8734d2a5bca2f582507f8b70db3922b16
engelmi <engel.michael@hm.edu>
2017-12-11 13:39:28 +0100

    Refactoring and preprocessing (zero center)

--- stockprediciton/preprocessing.py
+++ stockprediciton/preprocessing.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from stock import Stock
 
 from os import listdir
@@ -5,6 +7,36 @@ from os.path import isfile, join
 
 import numpy as np
 import csv
+
+"""
+Zero centers the stock and google trends data for a given csv file. 
+:param filepath: The path to the csv file to read and zero center the data. 
+:return: A zero centered numpy matrix containing the stock and google trends numbers. 
+
+TODO: exclude the labels (-1, 0, 1) from the mean calculation (column 1)
+"""
+def zeroCenter(filepath):
+	data = readMergedCSV(filepath)
+	data -= np.mean(data, axis=0)
+	return data
+
+"""
+Reads a csv file and returns a 2D array. 
+:param filepath: The path to the csv file. 
+:return: A numpy matrix.  
+"""
+def readMergedCSV(filepath):
+	data = []
+	try:
+		if isfile(filepath) and filepath[-3:] == "csv":
+			with open(filepath) as f:
+				reader = csv.reader(f, delimiter=',', quotechar='"')
+				for row in reader:
+					data.append([float(row[i]) for i in range(len(row))])
+	except Exception as ex:
+		# print ex.message
+		pass
+	return np.matrix(data)
 
 
 """
--- stockprediciton/stockprediciton.py
+++ stockprediciton/stockprediciton.py
@@ -1,32 +1,84 @@
-from . import preprocessing as pp
+# -*- coding: utf-8 -*-
+
+from os import listdir
+from os.path import isfile, join
+
+import preprocessing as pp
 
 import os
+import csv
 import tensorflow as tf
 import numpy as np
 
 """
 Main class for the prediction of stock course development. 
-TODO:   + Documentation
 """
 class StockPredictor(object):
 
-    def __init__(self):
-        pass
+	"""
+	Initialization. 
+	"""
+	def __init__(self):
+		self.folderMergedData = "datasets/myDataset"
+		self.folderPreprocessedData = "datasets/preprocessed"
 
+	"""
+	Creates a BasicLSTMCell with a given size of units. 
+	:param lstm_size: The size of the cell.
+	"""
+	def createLstmCell(lstm_size):
+  		return tf.contrib.rnn.BasicLSTMCell(lstm_size)
+	
+	"""
+	Preprocessing of the merged data. Reads the merged data of stocks and google trends, 
+	applies preprocessing routines to it and writes the result out. 
     """
-    ToDo:   + Documentation
-    """
-    def preprocessing(self):
-        stock = pp.loadSingleStock(os.path.join("datasets", "sandp500", "individual_stocks_5yr", "AAL_data.csv"))
-        print(stock.getStockName())
-        for d in stock.getDates():
-            print(d, stock.getDay(d))
+	def preprocessing(self):
+		deletePreprocessedData(self.folderPreprocessedData)
+		mergedCSVFiles = [f for f in listdir(self.folderMergedData) if isfile(join(self.folderMergedData, f)) and f[-3:] == "csv"]
+		for csvfile in mergedCSVFiles:
+			centeredData = pp.zeroCenter(join(self.folderMergedData, csvfile))
+			# maybe further preprocessing...
+			self.persistPreprocessedData(centeredData, self.folderPreprocessedData, csvfile[:len(csvfile)-4])
+			
+	"""
+	Persists a numpy matrix containing preprocessed data. 
+	:param matrix: The numpy matrix to persist. 
+	:param filepath: The relative path. 
+	:param filename: The name of the file. 
+	"""
+	def persistPreprocessedData(self, matrix, filepath, filename):
+		if not os.path.exists(filepath):
+			os.makedirs(filepath)
+		np.save(join(filepath, filename), matrix)
 
-        allStocks = pp.loadAllStockData(os.path.join("datasets", "sandp500", "individual_stocks_5yr"))
-        print(list(allStocks.keys()))
+	"""
+	Deletes the saved preprocessed data. 
+	:param filepath: The relative path. 
+	"""
+	def deletePreprocessedData(self, filepath):
+		if os.path.exists(filepath):
+			shutil.rmtree(filepath)
+
+	"""
+	Train the model. 
+	:param dataIsPreprocessed: Boolean flag to indicate data is already preprocessed. 
+	"""
+	def train(self, doPreprocessing):
+		if doPreprocessing:
+			preprocessing()
+		
+		return
+
+	"""
+	Predicts the course of a given stock. 
+	:param stock: The stock whose development shall be predicted. 
+	"""
+	def predict(self, stock):
+		pass
 
 
 
 if __name__ == "__main__":
-    sp = StockPredictor()
-    sp.preprocessing()
+	sp = StockPredictor()
+	sp.train(True)
