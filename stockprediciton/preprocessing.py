fc1ae46fddcd5abc7ef50e22f2ccfe455f149291
engelmi <engel.michael@hm.edu>
2017-12-12 14:06:15 +0100

    comment out unused methods

--- stockprediciton/preprocessing.py
+++ stockprediciton/preprocessing.py
@@ -7,13 +7,17 @@ from os.path import isfile, join
 
 import numpy as np
 import csv
+import sys
+
+# flag to check if python3 is used
+PY3 = sys.version_info[0] == 3
 
 """
 Zero centers the stock and google trends data for a given csv file. 
 :param filepath: The path to the csv file to read and zero center the data. 
 :return: A zero centered numpy matrix containing the stock and google trends numbers. 
 
-TODO: exclude the labels (-1, 0, 1) from the mean calculation (column 1)
+TODO: exclude the labels (-1, 0, 1) from the mean calculation (column 1)?!
 """
 def zeroCenter(filepath):
 	data = readMergedCSV(filepath)
@@ -39,38 +43,38 @@ def readMergedCSV(filepath):
 	return np.matrix(data)
 
 
-"""
-Iterates over all .csv files within the given folder. 
-
-ToDo:   + error handling
-        + extend function params -> file extension
-"""
-def loadAllStockData(relativeFolderPath):
-    allStocks = {}
-    csvfiles = [f for f in listdir(relativeFolderPath) if isfile(join(relativeFolderPath, f)) and f[-3:] == "csv"]
-    for f in csvfiles:
-        stock = loadSingleStock(join(relativeFolderPath, f))
-        if stock is not None:
-            allStocks[stock.getStockName()] = stock
-    return allStocks
-
-"""
-ToDo: + Exception handling -> additional logging?
-"""
-def loadSingleStock(relativePath):
-    stock = None
-    try:
-        csvfile = open(relativePath, 'rb')
-        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        csvHeader = reader.next()
-        currDate, currOpen, currHigh, currLow, currClose, currVolume, currName = reader.next()
-        stock = Stock(currName)
-        stock.addDay(currDate, currOpen, currHigh, currLow, currClose, currVolume, currName)
-        for rowCurrentDay in reader:
-            currDate, currOpen, currHigh, currLow, currClose, currVolume, currName = rowCurrentDay
-            stock.addDay(currDate, currOpen, currHigh, currLow, currClose, currVolume, currName)
-
-    except Exception as ex:
-        return stock
-
-    return stock
+#"""
+#Iterates over all .csv files within the given folder. 
+#
+#ToDo:   + error handling
+#        + extend function params -> file extension
+#"""
+#def loadAllStockData(relativeFolderPath):
+#    allStocks = {}
+#    csvfiles = [f for f in listdir(relativeFolderPath) if isfile(join(relativeFolderPath, f)) and f[-3:] == "csv"]
+#    for f in csvfiles:
+#        stock = loadSingleStock(join(relativeFolderPath, f))
+#        if stock is not None:
+#            allStocks[stock.getStockName()] = stock
+#    return allStocks
+#
+#"""
+#ToDo: + Exception handling -> additional logging?
+#"""
+#def loadSingleStock(relativePath):
+#    stock = None
+#    try:
+#        csvfile = open(relativePath, 'rb')
+#        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+#        csvHeader = reader.next()
+#        currDate, currOpen, currHigh, currLow, currClose, currVolume, currName = reader.next()
+#        stock = Stock(currName)
+#        stock.addDay(currDate, currOpen, currHigh, currLow, currClose, currVolume, currName)
+#        for rowCurrentDay in reader:
+#            currDate, currOpen, currHigh, currLow, currClose, currVolume, currName = rowCurrentDay
+#            stock.addDay(currDate, currOpen, currHigh, currLow, currClose, currVolume, currName)
+#
+#    except Exception as ex:
+#        return stock
+#
+#    return stock
